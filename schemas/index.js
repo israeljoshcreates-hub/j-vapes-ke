816f97d40a6bf5791306f7505dabd592c38a7c84
israeljoshcreates-hub <israeljoshcreates@gmail.com>
2025-12-29 01:53:03 +0300

    Restored clean version

--- schemas/index.js
+++ schemas/index.js
@@ -1,3 +1,4 @@
-import disposable from './disposable'
+import {disposable} from './disposable'
+import promo from './promo' // Import the new file
 
-export const schemaTypes = [disposable]
+export const schemaTypes = [disposable, promo] // Add it to the list
